78ba7cf8179e3c903f78b6c5b6b2476f1c001471
julia-castro <julia.castro@outlook.com>
2017-03-22 12:02:58 -0400

    creating the database

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -29,3 +29,14 @@ def get_db():
 def close_db(error):
   if hasattr(g, 'sqlite_db'):
     g.sqlite_db.close()
+
+def init_db():
+  db = get_db()
+  with app.open_resource('schema.sql', mode='r') as f:
+    db.cursor().executescript(f.read())
+  db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+  init_db()
+  print('Initialized the database')
