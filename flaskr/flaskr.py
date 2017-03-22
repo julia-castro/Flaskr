5f640f50acc23919a2115e32a0561bea70d6aaf0
julia-castro <julia.castro@outlook.com>
2017-03-22 11:51:02 -0400

    database connections

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -19,3 +19,13 @@ def connect_db():
   rv = sqlite3.connect(app.config['DATABASE'])
   rv.row_factory = sqlite3.Row
   return rv
+
+def get_db():
+  if not hasattr(g, 'sqlite_db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
